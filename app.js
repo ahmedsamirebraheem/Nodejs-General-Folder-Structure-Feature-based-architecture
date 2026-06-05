6481f6d58f0dfea032fe527242f56306afea97bf
Ahmed Samir <Ahmed Samir@DESKTOP-ST6FGPB>
2026-06-05 12:29:28 +0300

    chore: implement environment management and finalize architecture documentation

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 const express = require('express');
+const config = require('./src/config'); // استدعاء الكونفيج
 const productRoutes = require('./src/features/products/product.routes');
-require('dotenv').config();
+const errorHandler = require('./src/middlewares/errorHandler');
 
 const app = express();
 
-// Built-in Middlewares عشان يفهم الـ JSON requests
 app.use(express.json());
 
-// Base Route للتأكيد إن السيرفر قايم
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Node.js Feature-Based API is running perfectly!' });
 });
 
-
 app.use('/api/products', productRoutes);
-const errorHandler = require('./src/middlewares/errorHandler');
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+// استخدام الـ config.port بدل process.env
+app.listen(config.port, () => {
+    console.log(`Server is running in ${config.nodeEnv} mode on port ${config.port}`);
 });
---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+module.exports = {
+  port: process.env.PORT || 3000,
+  nodeEnv: process.env.NODE_ENV || 'development',
+  dbUrl: process.env.DB_URL,
+  jwtSecret: process.env.JWT_SECRET
+};
